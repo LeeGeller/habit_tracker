e5f2d01e30378e4b4044a891c330932d693db30a
Kris <ssspers92@gmail.com>
2024-07-08 19:44:51 +0300

    Upgrade in tasks.py remainder_habit

--- habits/tasks.py
+++ habits/tasks.py
@@ -2,21 +2,36 @@ from datetime import timedelta
 
 from celery import shared_task
 from django.utils import timezone
+from django_celery_beat.models import CrontabSchedule, PeriodicTask
 
-from habits.telegram import remind_about_habit
+from habits.telegram import bot
 from users.models import User
 
 
 @shared_task
 def remainder_habit():
     now_time = timezone.now()
-    habit_time = now_time + timedelta(minutes=30)
+    habit_time = now_time + timedelta(hours=3, minutes=30)
 
-    users_with_active_habits = User.objects.filter(habits__time_for_habit=habit_time)
-
-    habits = dict()
+    users_with_active_habits = User.objects.filter(habits__time_for_habit__gt=habit_time)
 
     for user in users_with_active_habits:
-        habits['user.id'] = user
+        name = f'reminder_habit_{user.id}_{user.habits.id}'
 
-    remind_about_habit.delay(habits)
+        schedule, created = CrontabSchedule.objects.get_or_create(
+            minute=user.time_for_habit.minute,
+            hour=user.time_for_habit.hour,
+            day_of_week='*',
+            day_of_month='*',
+            month_of_year='*',
+        )
+
+        PeriodicTask.objects.update_or_create(
+            crontab=schedule,
+            name=name,
+            defaults={
+                'task': 'habits.tasks.remainder_habit',
+                'args': f'[{user.id}, {user.habits.id}]',
+            }
+        )
+        bot.send_message(user.tg_id, f"I will {user.habits.action} at {user.time_for_habit} in {user.habits.place}")
--- habits/telegram.py
+++ habits/telegram.py
@@ -4,6 +4,7 @@ from aiogram.client.telegram import TelegramAPIServer
 from aiogram.filters import Command, CommandStart
 
 from config.settings import BOT_TOKEN, BOT_SERVER
+from users.models import User
 
 session = AiohttpSession(api=TelegramAPIServer.from_base(BOT_SERVER))
 bot = Bot(BOT_TOKEN, session=session)
@@ -13,18 +14,15 @@ router = Router()
 
 @dp.message(CommandStart())
 async def send_welcome_message(message: types.Message):
-    print(message)
     await message.answer(f"Hi, {message.from_user.full_name}! I'm your habit-bot.")
 
 
 @router.message()
-async def remind_about_habit(habit_dict):
-
-    for user_id, habits in habit_dict.items():
-        await bot.send_message(
-            user_id,
-            f"I will {habits.action} at {habits.time_to_complete} in {habits.place}",
-        )
+async def remind_about_habit(user_id, habit):
+    await bot.send_message(
+        user_id,
+        f"I will {habit.action} at {habit.time_to_complete} in {habit.place}",
+    )
 
 
 dp.include_router(router)
