cc1f2f88c6e451e93de9806903f96782d4d67fae
Kris <ssspers92@gmail.com>
2024-07-15 21:21:54 +0300

    Completed with swagger

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,8 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
+        # "rest_framework.permissions.IsAuthenticated",
+        "rest_framework.permissions.AllowAny",
     ],
 }
 SIMPLE_JWT = {
